aa939307d1a3bcc167c1d0976fc00c79e6d9f7f3
Marc Zottner <marc@zottner.com>
2024-02-15 08:59:14 +0100

    Minor version increment as many important features added

--- _versions.sh
+++ _versions.sh
@@ -7,7 +7,7 @@
 ##############################################################################################################
 
 # Current version of Application Portfolio Auditor
-export TOOL_VERSION='2.0.6'
+export TOOL_VERSION='2.1.0'
 
 # Java version used for Bagger and Fernflower
 export JAVA_VERSION='20'
