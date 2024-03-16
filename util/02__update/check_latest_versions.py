7d60835f182b3af2db6bba79e066dc89b645eec2
Marc Zottner <marc@zottner.com>
2024-03-16 12:09:47 +0100

    mc

--- util/02__update/check_latest_versions.py
+++ util/02__update/check_latest_versions.py
@@ -175,7 +175,7 @@ if __name__ == '__main__':
         (print_bold_message, '', r'', 'Application Analysis Tools', '', None),
         (check_github, 'pmd/pmd', r'.*pmd_releases/(.+)', 'PMD', PMD_VERSION, None),
         (check_github, 'albfernandez/GDS-PMD-Security-Rules', r'.*/tag/v\.(.+)', 'PMD GDS', PMD_GDS_VERSION, None),
-        (check_github, 'jeremylong/DependencyCheck', r'.*/tag/v(.+)', 'OWASP DC', OWASP_DC_VERSION, '9.0.9'),
+        (check_github, 'jeremylong/DependencyCheck', r'.*/tag/v(.+)', 'OWASP DC', OWASP_DC_VERSION, '9.0.10'),
         (check_github, 'find-sec-bugs/find-sec-bugs', r'.*/tag/version-(.+)', 'Find Security Bugs', FSB_VERSION, '1.13.0'),
         (check_github, 'nexB/scancode-toolkit', r'.*/tag/v(.+)', 'ScanCode', SCANCODE_VERSION, None),
         (check_github, 'AlDanial/cloc', r'.*/tag/v(.+)', 'CLOC', CLOC_VERSION, '2.00'),
