fbb5decbbf160af4e52f8870965047107efe8ce4
Marc Zottner <marc@zottner.com>
2024-04-03 08:36:06 +0200

    Extending validation script

--- util/03__validate/check_reports.sh
+++ util/03__validate/check_reports.sh
@@ -14,6 +14,7 @@ RED='\033[0;31m'
 ORANGE='\033[0;33m'
 BLUE='\033[0;36m'
 NORMAL='\033[0m'
+BOLD='\033[1m'
 
 readonly REPORT_FOLDER GREEN RED ORANGE BLUE NORMAL
 
@@ -23,8 +24,10 @@ function display_duration() {
 	local TOTAL_SECONDS=0
 
 	while read -r FILE; do
-		APP_GROUP=$(dirname "${FILE}" | rev | cut -d '_' -f 1 | rev)
-		echo ">>>>>>>> ${APP_GROUP} - ${FILE}"
+		REPORT_DIR="$(basename "$(dirname "${FILE}")")"
+		APP_GROUP=$(basename "${REPORT_DIR}" | rev | cut -d '_' -f 1 | rev)
+		TIMESTAMP=$(basename "${REPORT_DIR}" | rev | cut -d '_' -f 3- | rev)
+		echo "[${TIMESTAMP}] ${APP_GROUP} - ${FILE}"
 		TMP_FILE="${REPORT_FOLDER}/tmp"
 
 		# Extract first and last non empty lines
@@ -48,16 +51,16 @@ function display_duration() {
 			SECONDS=$((time_difference % 60))
 
 			# Print the result
-			echo -e "${APP_GROUP} - ${GREEN}Duration: $HOURS hours, $MINUTES minutes, $SECONDS seconds${NORMAL} >>> ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
+			echo -e "  >>>  ${GREEN}Duration: $HOURS hours, $MINUTES minutes, $SECONDS seconds${NORMAL} >>> ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
 			(( TOTAL_HOURS += HOURS ))
 			(( TOTAL_MINUTES += MINUTES ))
 			(( TOTAL_SECONDS += SECONDS ))
 		else
-			echo -e "${APP_GROUP} - ${RED}No time data found${NORMAL} >>> ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
+			echo -e "  >>>  ${RED}No time data found${NORMAL} >>> ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
 		fi
 		rm -f "${TMP_FILE}"
 
-	done < <(find "${REPORT_FOLDER}" -mindepth 2 -maxdepth 2 -type f -name '*_timeline.log')
+	done < <(find "${REPORT_FOLDER}" -mindepth 2 -maxdepth 2 -type f -name '*_timeline.log'|sort -f)
 
 	# Convert total seconds to minutes and add to total minutes
 	(( "TOTAL_MINUTES += TOTAL_SECONDS / 60" )) 
@@ -75,4 +78,27 @@ function display_duration() {
 	echo -e "Total time: ${ORANGE}$TOTAL_HOURS hours, $TOTAL_MINUTES minutes, $TOTAL_SECONDS seconds${NORMAL}"
 }
 
-display_duration
+function analyze_logs() {
+
+	while read -r REPORT_DIR; do
+		APP_GROUP=$(basename "${REPORT_DIR}" | rev | cut -d '_' -f 1 | rev)
+		TIMESTAMP=$(basename "${REPORT_DIR}" | rev | cut -d '_' -f 3- | rev)
+		echo -e "[${TIMESTAMP}] ${APP_GROUP}"
+		while read -r FSB_LOG; do
+			if grep -q '^Out of memory' "${FSB_LOG}"; then
+				echo -e "  >>>  ${RED}FindSecBugs - 'Out of memory' error detected${NORMAL} - ${FSB_LOG}"
+			fi
+		done < <(find "${REPORT_DIR}" -mindepth 1 -maxdepth 1 -type f -name '*FindSecBugs*.log')
+
+	done < <(find "${REPORT_FOLDER}" -mindepth 1 -maxdepth 1 -type d -name '20*'|sort -f)
+}
+
+function main() {
+	echo -e "${BOLD}Duration${NORMAL}\n"
+	display_duration
+
+	echo -e "\n${BOLD}Logs${NORMAL}\n"
+	analyze_logs
+} 
+
+main
