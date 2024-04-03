6caea568f3e9827c5a496d58f338ac965d326d1f
Marc Zottner <marc@zottner.com>
2024-04-03 09:32:38 +0200

    Improving resiliency

--- util/03__validate/check_reports.sh
+++ util/03__validate/check_reports.sh
@@ -33,46 +33,55 @@ function display_duration() {
 		# Extract first and last non empty lines
 		grep -v '^[[:space:]]*$' "${FILE}" | sed -e 1b -e '$!d' >"${TMP_FILE}"
 
-		# Extract formatted date strings
-		date_str1=$(grep -o '\[[0-9_]*\]' "${TMP_FILE}" | sed -n 1p | awk -F'[_\\[\\]]' '{print $2"-"$3"-"$4"__"$6"_"$7"_"$8}')
-		date_str2=$(grep -o '\[[0-9_]*\]' "${TMP_FILE}" | sed -n 2p | awk -F'[_\\[\\]]' '{print $2"-"$3"-"$4"__"$6"_"$7"_"$8}')
+		line1=$(grep -o '\[[0-9_]*\]' "${TMP_FILE}" | sed -n 1p)
+		line2=$(grep -o '\[[0-9_]*\]' "${TMP_FILE}" | sed -n 2p)
 
-		if [[ -n "${date_str1}${date_str2}" ]]; then
-			# Convert formatted dates to UNIX timestamps
-			timestamp1=$(date -j -f "%Y-%m-%d__%H_%M_%S" "${date_str1}" "+%s")
-			timestamp2=$(date -j -f "%Y-%m-%d__%H_%M_%S" "${date_str2}" "+%s")
+		if [[ -n "${line1}" ]] && [[ -n "${line2}" ]]; then
+			# Extract formatted date strings
+			date_str1=$(echo " ${line1}" | awk -F'[_\\[\\]]' '{print $2"-"$3"-"$4"__"$6"_"$7"_"$8}')
+			date_str2=$(echo " ${line2}" | awk -F'[_\\[\\]]' '{print $2"-"$3"-"$4"__"$6"_"$7"_"$8}')
 
-			# Calculate the time difference in seconds
-			time_difference=$((timestamp2 - timestamp1))
+			if [[ -n "${date_str1}" ]] && [[ -n "${date_str2}" ]]; then
+				# Convert formatted dates to UNIX timestamps
+				timestamp1=$(date -j -f "%Y-%m-%d__%H_%M_%S" "${date_str1}" "+%s")
+				timestamp2=$(date -j -f "%Y-%m-%d__%H_%M_%S" "${date_str2}" "+%s")
 
-			# Calculate hours, minutes, and seconds
-			HOURS=$((time_difference / 3600))
-			MINUTES=$(((time_difference % 3600) / 60))
-			SECONDS=$((time_difference % 60))
+				# Calculate the time difference in seconds
+				time_difference=$((timestamp2 - timestamp1))
 
-			# Print the result
-			echo -e "  >>>  ${GREEN}Duration: $HOURS hours, $MINUTES minutes, $SECONDS seconds${NORMAL} >>> ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
-			(( TOTAL_HOURS += HOURS ))
-			(( TOTAL_MINUTES += MINUTES ))
-			(( TOTAL_SECONDS += SECONDS ))
+				# Calculate hours, minutes, and seconds
+				HOURS=$((time_difference / 3600))
+				MINUTES=$(((time_difference % 3600) / 60))
+				SECONDS=$((time_difference % 60))
+
+				# Print the result
+				echo -e "  >>> ${GREEN}Duration: $HOURS hours, $MINUTES minutes, $SECONDS seconds${NORMAL} - ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
+				((TOTAL_HOURS += HOURS))
+				((TOTAL_MINUTES += MINUTES))
+				((TOTAL_SECONDS += SECONDS))
+
+			else
+				echo -e "  >>> ${RED}No time data found${NORMAL} - ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
+			fi
 		else
-			echo -e "  >>>  ${RED}No time data found${NORMAL} >>> ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
+			echo -e "  >>> ${RED}No time data found${NORMAL} - ${BLUE}$(dirname "${FILE}")/index.html${NORMAL}"
 		fi
+
 		rm -f "${TMP_FILE}"
 
-	done < <(find "${REPORT_FOLDER}" -mindepth 2 -maxdepth 2 -type f -name '*_timeline.log'|sort -f)
+	done < <(find "${REPORT_FOLDER}" -mindepth 2 -maxdepth 2 -type f -name '*_timeline.log' | sort -f)
 
 	# Convert total seconds to minutes and add to total minutes
-	(( "TOTAL_MINUTES += TOTAL_SECONDS / 60" )) 
+	(("TOTAL_MINUTES += TOTAL_SECONDS / 60"))
 
 	# Calculate remaining seconds after converting to minutes
-	(( "TOTAL_SECONDS %= 60" ))
+	(("TOTAL_SECONDS %= 60"))
 
 	# Convert total minutes to hours and add to total hours
-	(( "TOTAL_HOURS += TOTAL_MINUTES / 60" ))
+	(("TOTAL_HOURS += TOTAL_MINUTES / 60"))
 
 	# Calculate remaining minutes after converting to hours
-	(( "TOTAL_MINUTES %= 60" ))
+	(("TOTAL_MINUTES %= 60"))
 
 	echo ""
 	echo -e "Total time: ${ORANGE}$TOTAL_HOURS hours, $TOTAL_MINUTES minutes, $TOTAL_SECONDS seconds${NORMAL}"
@@ -90,7 +99,7 @@ function analyze_logs() {
 			fi
 		done < <(find "${REPORT_DIR}" -mindepth 1 -maxdepth 1 -type f -name '*FindSecBugs*.log')
 
-	done < <(find "${REPORT_FOLDER}" -mindepth 1 -maxdepth 1 -type d -name '20*'|sort -f)
+	done < <(find "${REPORT_FOLDER}" -mindepth 1 -maxdepth 1 -type d -name '20*' | sort -f)
 }
 
 function main() {
@@ -99,6 +108,6 @@ function main() {
 
 	echo -e "\n${BOLD}Logs${NORMAL}\n"
 	analyze_logs
-} 
+}
 
 main
