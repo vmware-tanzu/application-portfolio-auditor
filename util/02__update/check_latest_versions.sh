3fb69b68f9c3e64492ab33fe916b7e42ed548a86
Marc Zottner <marc@zottner.com>
2024-05-06 11:48:20 +0200

    Update "./audit check" command to run in a virtual python environment if python is installed

--- util/02__update/check_latest_versions.sh
+++ util/02__update/check_latest_versions.sh
@@ -14,17 +14,28 @@ SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 # shellcheck disable=SC1091
 source "${SCRIPT_DIR}/../../_shared_functions.sh"
 
-PYTHON_CMD=$(command -v python || command -v python3)
-PIP_CMD=$(command -v pip || command -v pip3)
+PYTHON_CMD_FULL=$(command -v python3 || command -v python)
 
-if [[ -z "${PYTHON_CMD}" ]] || [[ -z "${PIP_CMD}" ]]; then
-	[[ -z "${PYTHON_CMD}" ]] && echo_console_error "'python' is not available. Please install it."
-	[[ -z "${PIP_CMD}" ]] && echo_console_error "'pip' is not available. Please install it."
+if [[ -z "${PYTHON_CMD_FULL}" ]]; then
+	echo "This feature requires 'python' to be installed. Please install python and retry."
 	exit 1
 fi
 
-# Import all required python libraries
-${PIP_CMD} install bs4 requests aiohttp --break-system-packages >>/dev/null
+PYTHON_CMD=$(basename "${PYTHON_CMD_FULL}")
+
+# Create a Python virtual environment
+VENV_DIR="${SCRIPT_DIR}/.venv"
+if [[ ! -d "${VENV_DIR}" ]]; then
+	echo "Creating Python virtual environment..."
+	${PYTHON_CMD_FULL} -m venv "${VENV_DIR}"
+fi
+
+# Activate the virtual environment
+source "${VENV_DIR}/bin/activate"
+
+# Upgrade pip and import all required python libraries
+${PYTHON_CMD} -m pip --quiet install --upgrade pip
+${PYTHON_CMD} -m pip --quiet install BeautifulSoup4 requests aiohttp
 
 # Load the current version numbers
 # shellcheck source=/dev/null
@@ -33,3 +44,6 @@ source "${SCRIPT_DIR}/../../_versions.sh"
 # Check for new versions
 # shellcheck source=/dev/null
 ${PYTHON_CMD} "${SCRIPT_DIR}/check_latest_versions.py"
+
+# Deactivate the virtual environment
+deactivate
