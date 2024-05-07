c217d457d2f291a5bc33cd0f54a36ed00b789612
Marc Zottner <marc@zottner.com>
2024-05-07 21:12:04 +0200

    Incrementing tool version

--- _versions.sh
+++ _versions.sh
@@ -7,7 +7,7 @@
 ##############################################################################################################
 
 # Current version of Application Portfolio Auditor
-export TOOL_VERSION='2.2.3'
+export TOOL_VERSION='2.2.4'
 
 # List of the versions for all tools in use.
 
