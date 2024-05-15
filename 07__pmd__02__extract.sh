a62df5ac04bb53d7b52132d67dd3704e9307553a
Marc Zottner <marc@zottner.com>
2024-05-15 06:48:49 +0200

    Removing columns in the quality heatmap table: PMD rules, copy-paster lines & tokens

--- 07__pmd__02__extract.sh
+++ 07__pmd__02__extract.sh
@@ -27,8 +27,9 @@ declare -A LANGUAGES=(
 )
 
 function extract() {
-	rm -f "${RESULT_FILE}"
-	touch "${RESULT_FILE}"
+
+	#echo "Applications${SEPARATOR}PMD rules triggered${SEPARATOR}PMD violations${SEPARATOR}Copy-pasted fragments${SEPARATOR}Copy-pasted lines${SEPARATOR}Copy-pasted tokens"> "${RESULT_FILE}"
+	echo "Applications${SEPARATOR}PMD violations${SEPARATOR}Copy-pasted fragments" >"${RESULT_FILE}"
 
 	while read -r APP; do
 		APP_NAME=$(basename "${APP}")
@@ -42,65 +43,59 @@ function extract() {
 			fi
 		done
 
-		declare COUNT_VIOLATIONS COUNT_RULES
+		declare COUNT_VIOLATIONS
+		#declare COUNT_RULES
 		COUNT_VIOLATIONS='n/a'
-		COUNT_RULES='n/a'
+		#COUNT_RULES='n/a'
 		if [[ "${LANGUAGE}" == "Java" ]]; then
 			PMD_FILE=${PMD_DIR_OUT}/${APP_NAME}_pmd.html
 			if [[ -f "${PMD_FILE}" ]]; then
 				COUNT_VIOLATIONS=$(awk 'BEGIN { count=0 } /<td align="center">[0-9]*<\/td>/{count++} END{print count}' "${PMD_FILE}" || true)
-				COUNT_RULES=$(awk -F'[<>]' 'BEGIN { count=0 } /<tr><td>[^<]*<\/td><td align=center>[0-9]*<\/td><\/tr>/ {count++} END {print count}' "${PMD_FILE}" || true)
+				#COUNT_RULES=$(awk -F'[<>]' 'BEGIN { count=0 } /<tr><td>[^<]*<\/td><td align=center>[0-9]*<\/td><\/tr>/ {count++} END {print count}' "${PMD_FILE}" || true)
 			fi
-			#echo "${APP_NAME} - VIOLATIONS: ${COUNT_VIOLATIONS} - RULES: ${COUNT_RULES}"
 		fi
 
 		CPD_FILE=${CPD_DIR_OUT}/${APP_NAME}__cpd.xml
 
-		declare COUNT_DUPLICATED_FRAMENTS TOTAL_DUPLICATED_LINES TOTAL_DUPLICATED_TOKENS
+		declare COUNT_DUPLICATED_FRAGMENTS
+		#declare TOTAL_DUPLICATED_LINES TOTAL_DUPLICATED_TOKENS
 		if [[ -f "${CPD_FILE}" ]]; then
-			TOTAL_DUPLICATED_LINES=0
-			TOTAL_DUPLICATED_TOKENS=0
-			COUNT_DUPLICATED_FRAMENTS=$(awk 'BEGIN { count=0 } /<duplication lines="/ { count++ } END { print count }' "${CPD_FILE}" || true)
-			LINES=0
-			TOKENS=0
-			FIRST="true"
-			while IFS='' read -r LINE; do
-				#echo ${LINE}
-				if [[ "${LINE}" == *'<duplication lines="'* ]]; then
-					# shellcheck disable=SC2001
-					LINES=$(echo "${LINE}" | sed 's/<duplication lines="\([^"]*\)".*/\1/')
-					# shellcheck disable=SC2001
-					TOKENS=$(echo "${LINE}" | sed 's/.*tokens="\([^"]*\)">.*/\1/')
-					FIRST="true"
-					#echo "LINES: $LINES  -  TOKENS: $TOKENS"
-				elif [[ "${LINE}" =~ \<file\ [a-z]*\=\" ]]; then
-					if [[ "${FIRST}" == "true" ]]; then
-						FIRST="false"
-					else
-						# Counting duplicates from the second occurence
-						TOTAL_DUPLICATED_LINES=$((TOTAL_DUPLICATED_LINES + LINES))
-						TOTAL_DUPLICATED_TOKENS=$((TOTAL_DUPLICATED_TOKENS + TOKENS))
-						#echo "TOTAL_DUPLICATED_LINES: ${TOTAL_DUPLICATED_LINES}  -  TOTAL_DUPLICATED_TOKENS: ${TOTAL_DUPLICATED_TOKENS}"
-					fi
-				fi
-			done < <(awk '/<duplication lines="|<file [a-z]*=/' "${CPD_FILE}")
+			#TOTAL_DUPLICATED_LINES=0
+			#TOTAL_DUPLICATED_TOKENS=0
+			COUNT_DUPLICATED_FRAGMENTS=$(awk 'BEGIN { count=0 } /<duplication lines="/ { count++ } END { print count }' "${CPD_FILE}" || true)
+			#LINES=0
+			#TOKENS=0
+			#FIRST="true"
+			#while IFS='' read -r LINE; do
+			#	#echo ${LINE}
+			#	if [[ "${LINE}" == *'<duplication lines="'* ]]; then
+			#		# shellcheck disable=SC2001
+			#		LINES=$(echo "${LINE}" | sed 's/<duplication lines="\([^"]*\)".*/\1/')
+			#		# shellcheck disable=SC2001
+			#		TOKENS=$(echo "${LINE}" | sed 's/.*tokens="\([^"]*\)">.*/\1/')
+			#		FIRST="true"
+			#		#echo "LINES: $LINES  -  TOKENS: $TOKENS"
+			#	elif [[ "${LINE}" =~ \<file\ [a-z]*\=\" ]]; then
+			#		if [[ "${FIRST}" == "true" ]]; then
+			#			FIRST="false"
+			#		else
+			#			# Counting duplicates from the second occurence
+			#			TOTAL_DUPLICATED_LINES=$((TOTAL_DUPLICATED_LINES + LINES))
+			#			TOTAL_DUPLICATED_TOKENS=$((TOTAL_DUPLICATED_TOKENS + TOKENS))
+			#			#echo "TOTAL_DUPLICATED_LINES: ${TOTAL_DUPLICATED_LINES}  -  TOTAL_DUPLICATED_TOKENS: ${TOTAL_DUPLICATED_TOKENS}"
+			#		fi
+			#	fi
+			#done < <(awk '/<duplication lines="|<file [a-z]*=/' "${CPD_FILE}")
 		else
-			COUNT_DUPLICATED_FRAMENTS='n/a'
-			TOTAL_DUPLICATED_LINES='n/a'
-			TOTAL_DUPLICATED_TOKENS='n/a'
+			COUNT_DUPLICATED_FRAGMENTS='n/a'
+			#TOTAL_DUPLICATED_LINES='n/a'
+			#TOTAL_DUPLICATED_TOKENS='n/a'
 		fi
 
-		#echo "${APP_NAME} - DUPLICATED FRAGMENTS: ${COUNT_DUPLICATED_FRAMENTS} - LINES: ${TOTAL_DUPLICATED_LINES} - TOKENS: ${TOTAL_DUPLICATED_TOKENS}"
-		echo "${APP_NAME}${SEPARATOR}${COUNT_RULES}${SEPARATOR}${COUNT_VIOLATIONS}${SEPARATOR}${COUNT_DUPLICATED_FRAMENTS}${SEPARATOR}${TOTAL_DUPLICATED_LINES}${SEPARATOR}${TOTAL_DUPLICATED_TOKENS}" >>"${RESULT_FILE}"
+		#echo "${APP_NAME}${SEPARATOR}${COUNT_RULES}${SEPARATOR}${COUNT_VIOLATIONS}${SEPARATOR}${COUNT_DUPLICATED_FRAGMENTS}${SEPARATOR}${TOTAL_DUPLICATED_LINES}${SEPARATOR}${TOTAL_DUPLICATED_TOKENS}" >>"${RESULT_FILE}"
+		echo "${APP_NAME}${SEPARATOR}${COUNT_VIOLATIONS}${SEPARATOR}${COUNT_DUPLICATED_FRAGMENTS}" >>"${RESULT_FILE}"
 
 	done <"${REPORTS_DIR}/00__Weave/list__all_apps.txt"
-
-	# Adding the header
-	{
-		echo "Applications${SEPARATOR}PMD rules triggered${SEPARATOR}PMD violations${SEPARATOR}Copy-pasted fragments${SEPARATOR}Copy-pasted lines${SEPARATOR}Copy-pasted tokens"
-		cat "${RESULT_FILE}"
-	} >"${RESULT_FILE}.tmp"
-	mv "${RESULT_FILE}.tmp" "${RESULT_FILE}"
 }
 
 function main() {
